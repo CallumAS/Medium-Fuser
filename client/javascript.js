6f50a7ebcd026e5a245c4c2909d4bda7cc402352
CallumAS <53473235+CallumAS@users.noreply.github.com>
2023-05-29 19:40:38 +0100

    Componentise the widget

--- client/javascript.js
+++ client/javascript.js
@@ -1,126 +1,246 @@
 const styleElement = document.createElement('style');
 styleElement.textContent = `
-  body {
-    font-family: Arial, sans-serif;
-    background-color: #f5f5f5;
-  }
-
-  #widget {
-    width: 400px;
-    background-color: #fff;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    margin: 50px auto;
-    padding: 20px;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-  }
-
-  .post-container {
-    max-width: 600px;
-    margin: 10 auto;
-    font-family: Arial, sans-serif;
-    --tw-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
-    --tw-shadow-colored: 0 4px 6px -1px var(--tw-shadow-color), 0 2px 4px -2px var(--tw-shadow-color);
-    box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
-    --tw-bg-opacity: 1;
-    background-color: rgb(255 255 255 / var(--tw-bg-opacity));
-    border-width: 1px;
-    padding: 1.25rem;
-    border-radius: 0.75rem;
-  }
-
-  /* Style for the post title */
-  .post-title {
-    font-size: 24px;
-    margin-bottom: 10px;
-  }
-
-  /* Style for the creator section */
-  .creator-section {
-    display: flex;
-    align-items: center;
-    margin-bottom: 10px;
-    padding: 10px;
-    border-bottom: 2px solid #e5e7eb;
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f5f5f5;
+  margin: 0;
+  padding: 0;
 }
-  .creator-image {
-    width: 50px;
-    height: 50px;
-    border-radius: 50%;
-    margin-right: 10px;
-    flex-shrink: 0;
-  }
 
-  .creator-name {
-    font-size: 18px;
-    font-weight: bold;
-    margin-bottom: 0px;
-  }
+#widget {
+  width: 100%;
+  max-width: 500px;
+  margin: 50px auto;
+  padding: 20px;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+}
 
-  .creator-bio {
-    font-size: 14px;
-    color: #666;
-  }
+.post-container {
+  width: 100%;
+  max-width: 500px;
 
-  /* Style for the tags and timestamp */
-  .timestamp {
-    margin-bottom: 10px;
-    margin-left: 10px;
-    color: #777;
-  }
-  .tag {
-    font-weight: 600;
-    font-size: 0.75rem;
-    line-height: 1rem;
-    padding-top: 0.25rem;
-    padding-bottom: 0.25rem;
-    padding-left: 0.75rem;
-    padding-right: 0.75rem;
-    --tw-bg-opacity: 1;
-    background-color: rgb(245 245 245 / var(--tw-bg-opacity));
-    border-radius: 1rem;
-    border-width: 1px;
-    margin-bottom: 10px;
-    margin-left: 10px;
-    color: #777;
-  }
-  
 
-  /* Style for the content paragraphs */
-  .content-paragraph {
-    margin-bottom: 10px;
-    line-height: 1.5;
-  }
+  margin: 10px auto;
+  padding: 10px;
+  background-color: #fff;
+  border-radius: 5px;
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -2px rgba(0, 0, 0, 0.1);
+}
 
-  /* Style for image content */
-  .content-image {
-    max-width: 100%;
-    margin-bottom: 10px;
-  }
+.creator-section {
+  flex-wrap: wrap;
+  align-items: center;
+  margin-bottom: 10px;
+  padding: 10px;
+  border-bottom: 2px solid #e5e7eb;
+}
 
-  .show-more-link {
-    display: block;
-    text-align: center;
-    color: #333;
-    text-decoration: none;
-    font-size: 14px;
-    font-weight: bold;
-    padding: 8px 16px;
-    border: 2px solid #e5e7eb;
-    border-radius: 20px;
-    --tw-bg-opacity: 1;
-    background-color: rgb(245 245 245 / var(--tw-bg-opacity));
+.creator-image {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin-right: 10px;
+  flex-shrink: 0;
+}
 
-    transition: background-color 0.3s, color 0.3s;
-  }
+.creator-name {
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 0;
+}
 
-  .show-more-link:hover {
-    background-color: #e5e7eb;
-  }
+.creator-bio {
+  font-size: 14px;
+  color: #666;
+}
+
+.timestamp {
+  margin-bottom: 10px;
+  margin-left: auto;
+  color: #777;
+}
+
+.tag {
+  font-weight: 600;
+  font-size: 0.75rem;
+  line-height: 1rem;
+  padding: 0.25rem 0.75rem;
+  background-color: #f5f5f5;
+  border-radius: 1rem;
+  border-width: 1px;
+  margin-bottom: 10px;
+  margin-left: 10px;
+  color: #777;
+  white-space: nowrap;
+}
+
+.content-paragraph {
+  margin-bottom: 10px;
+  line-height: 1.5;
+}
+
+.content-image {
+  max-width: 100%;
+  height: auto;
+  margin-bottom: 10px;
+}
+
+.show-more-link {
+  display: block;
+  text-align: center;
+  color: #333;
+  text-decoration: none;
+  font-size: 14px;
+  font-weight: bold;
+  padding: 8px 16px;
+  border: 2px solid #e5e7eb;
+  border-radius: 20px;
+  background-color: #f5f5f5;
+  transition: background-color 0.3s, color 0.3s;
+}
+
+.show-more-link:hover {
+  background-color: #e5e7eb;
+}
+
+.article {
+
+  transition:height 0.3s ease-in-out;
+  transition-delay: 0.1s;
+   overflow: hidden;
+}
 `;
 
 document.head.appendChild(styleElement);
 
+class MediumPost extends HTMLElement {
+  constructor() {
+    super();
+    this.showMore = false;
+    this.content = '';
+    this.postContent = null //post div
+    this.showButton = null; //show button
+    this.titleBar = null;
+    this.Article = null; //article
+    this.creatorSection = null;//creator header
+  }
+
+  connectedCallback() {
+    this.renderPost();
+  }
+  formatTime(timestamp) {
+    const currentTime = new Date().getTime();
+    const timeDiff = currentTime - timestamp;
+
+    const minute = 60 * 1000;
+    const hour = 60 * minute;
+    const day = 24 * hour;
+
+    if (timeDiff < minute) {
+      return `${Math.floor(timeDiff / 1000)} seconds ago`;
+    } else if (timeDiff < hour) {
+      return `${Math.floor(timeDiff / minute)} minutes ago`;
+    } else if (timeDiff < day) {
+      return `${Math.floor(timeDiff / hour)} hours ago`;
+    } else {
+      const date = new Date(timestamp);
+      const year = date.getFullYear();
+      const month = date.getMonth() + 1;
+      const dayOfMonth = date.getDate();
+
+      return `${month}/${dayOfMonth}/${year}`;
+    }
+  }
+  renderPost() {
+    if (this.postContent == null) {
+      this.postContent = document.createElement("div");
+      this.postContent.className = "post-container";
+    }
+    if (this.creatorSection == null) {
+
+      this.creatorSection = document.createElement("div");
+      this.creatorSection.className = "creator-section";
+      var headerCard = document.createElement("div");
+      headerCard.style = "display: flex;"
+
+      var profileImg = document.createElement("img");
+      profileImg.className = "creator-image"
+      profileImg.alt = "creator-image"
+      profileImg.src = "https://miro.medium.com/v2/resize:fit:1400/format:webp/" + this.content.Creator.Image;
+      headerCard.appendChild(profileImg)
+
+      var profileName = document.createElement("h2");
+      profileName.className = "creator-name"
+      profileName.innerText = this.content.Creator.Name;
+      headerCard.appendChild(profileName)
+
+
+      var profileBio = document.createElement("p");
+      profileBio.className = "creator-bio"
+      profileBio.innerText = this.content.Creator.Bio;
+
+      this.creatorSection.appendChild(headerCard)
+      this.creatorSection.appendChild(profileBio)
+
+      this.postContent.appendChild(this.creatorSection);
+
+
+    }
+
+    if (this.titleBar == null) {
+      this.titleBar = document.createElement("div");
+      this.titleBar.style = "display: flex; align-items: baseline;"
+      this.titleBar.innerHTML = `  <h1 class="post-title">
+      ${this.content.Title}
+      <p class="timestamp" style="display: inline;margin-left: 10px;font-weight: normal;font-size: medium;">Posted ${this.formatTime(this.content.CreatedAt)}</p>
+    </h1>`
+      this.postContent.appendChild(this.titleBar)
+    }
+
+
+    if (this.Article == null) {
+      this.Article = document.createElement("div")
+      this.Article.className = "article"
+      this.postContent.appendChild(this.Article)
+    }
+    this.Article.innerHTML = `${this.content.Content
+      .slice(2, this.showMore ? this.content.Content.length - 2 : 5)
+      .map(
+        (content, index) =>
+          `<${content.type} class="${content.type === "IMG" ? "content-image" : "content-paragraph"
+          }"${content.type === "IMG"
+            ? `src="https://miro.medium.com/v2/resize:fit:1400/format:webp/${content.image}"`
+            : ""
+          }>${content.type !== "IMG" ? content.text + (index === 2 && this.content.Content.length > 5 && !this.showMore ? " ..." : "") : ""}</${content.type}>${content.type === "IMG" ? "<br/>" : ""
+          }`
+      )
+      .join("")}`;
+
+
+
+    if (this.showButton == null) {
+
+
+      this.showButton = document.createElement("a");
+      this.showButton.className = "show-more-link"
+      this.showButton.onclick = () => {
+        this.showMore = !this.showMore;
+        this.renderPost(); // Update the post content when showMore changes
+      };
+      this.postContent.appendChild(this.showButton);
+    }
+    this.showButton.innerText = this.showMore ? "Show Less" : "Show More";
+    this.appendChild(this.postContent)
+
+
+
+
+
+  }
+}
 
 class MediumWidget extends HTMLElement {
   constructor() {
@@ -136,7 +256,6 @@ class MediumWidget extends HTMLElement {
 
     // Add scroll event listener
     window.addEventListener("scroll", this.handleScroll.bind(this));
-    this.addEventListener("click", this.handleShowMoreClick);
 
   }
 
@@ -174,100 +293,13 @@ class MediumWidget extends HTMLElement {
     }
   }
 
-  formatTime(timestamp) {
-    const currentTime = new Date().getTime();
-    const timeDiff = currentTime - timestamp;
-
-    const minute = 60 * 1000;
-    const hour = 60 * minute;
-    const day = 24 * hour;
-
-    if (timeDiff < minute) {
-      return `${Math.floor(timeDiff / 1000)} seconds ago`;
-    } else if (timeDiff < hour) {
-      return `${Math.floor(timeDiff / minute)} minutes ago`;
-    } else if (timeDiff < day) {
-      return `${Math.floor(timeDiff / hour)} hours ago`;
-    } else {
-      const date = new Date(timestamp);
-      const year = date.getFullYear();
-      const month = date.getMonth() + 1;
-      const dayOfMonth = date.getDate();
-
-      return `${month}/${dayOfMonth}/${year}`;
-    }
-  }
-
-  handleShowMoreClick(event) {
-    const target = event.target;
-    if (target.classList.contains("show-more-link")) {
-      const slug = target.getAttribute("data-slug");
-      this.ShowMore(slug);
-      event.preventDefault();
-    }
-  }
-
-  ShowMore(slug) {
-    const contentSection = this.ChildrenPosts.get(slug).element.querySelector(".content-section");
-    const post = this.ChildrenPosts.get(slug).data;
-
-    contentSection.innerHTML = `${post.Content.slice(2, post.Content.length - 2)
-      .map(
-        (content) =>
-          `<${content.type} class="${content.type === "IMG" ? "content-image" : "content-paragraph"
-          }"${content.type === "IMG"
-            ? `src="https://miro.medium.com/v2/resize:fit:1400/format:webp/${content.image}"`
-            : ""
-          }>${content.type !== "IMG" ? content.text : ""}</${content.type}>${content.type === "IMG" ? "<br/>" : ""
-          }`
-      )
-      .join("")}`;
-  }
-
   renderPost(post) {
-    const postContainer = document.createElement("div");
-    postContainer.className = "post-container";
-
-    postContainer.innerHTML = `
-        <div class="creator-section">
-        <img class="creator-image" src="https://miro.medium.com/v2/resize:fit:1400/format:webp/${post.Creator.Image}" alt="Creator Image">
-        <div>
-          <h2 class="creator-name">${post.Creator.Name}</h2>
-          <p class="creator-bio">${post.Creator.Bio}</p>
-        </div>
-      </div>
-      <div style="display: flex; align-items: baseline;">
-      <h1 class="post-title">
-      ${post.Title}
-      <p class="timestamp" style="display: inline;margin-left: 10px;font-weight: normal;font-size: medium;">Posted ${this.formatTime(post.CreatedAt)}</p>
-    </h1>
-      </div>
-
-      <div style="display: flex; align-items: baseline;">
-      ${post.Tags.map(tag => `<p class="tag">${tag}</p>`).join("")}
-
-      </div>
-
-          <div class="content-section">
-            ${post.Content
-        .slice(2, 5)
-        .map(
-          (content, index) =>
-            `<${content.type} class="${content.type === "IMG" ? "content-image" : "content-paragraph"
-            }"${content.type === "IMG"
-              ? `src="https://miro.medium.com/v2/resize:fit:1400/format:webp/${content.image}"`
-              : ""
-            }>${content.type !== "IMG" ? content.text + (index === 2 && post.Content.length > 5 ? " ..." : "") : ""}</${content.type}>${content.type === "IMG" ? "<br/>" : ""
-            }`
-        )
-        .join("")}
-                ${post.Content.length > 5 ? `<br/><a href="#" class="show-more-link" data-slug="${post.Slug}">SHOW MORE</a>` : ""}
-                </div>
-        `;
-
-    this.ChildrenPosts.set(post.Slug, { data: post, element: postContainer });
-    this.appendChild(postContainer);
+    const test = document.createElement("medium-post");
+    test.content = post
+    this.appendChild(test)
   }
 }
 
 customElements.define("medium-widget", MediumWidget);
+
+customElements.define("medium-post", MediumPost);
